a57ecb7371a621666350cd64c9b33e04a18e8394
williamsoftwarecode <williamsoftwarecode@gmail.com>
2019-06-18 23:30:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/demo-deploy'));
 
 app.get('/*', function(req,res) {
-
 res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
 });
 
